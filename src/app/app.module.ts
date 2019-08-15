616b0b3bafc1879dbc2ab0ad3514ad665869003a
Leoshan Reddi <leoshanreddi@Leoshans-MacBook-Pro.local>
2019-08-15 08:56:10 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { TodoComponent } from './todo/todo.component';
+import { TodoService } from './todo/Shared/todo.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
-  imports: [
+  imports: [ 
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
+
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA-xJXqeotODbX4OTxaonqMH6Z8tVzTYXQ",
+    authDomain: "angular-enemy-list-app.firebaseapp.com",
+    databaseURL: "https://angular-enemy-list-app.firebaseio.com",
+    projectId: "angular-enemy-list-app",
+    storageBucket: "angular-enemy-list-app.appspot.com",
+    messagingSenderId: "599129685664",
+    appId: "1:599129685664:web:7a1d2f2171c9709b"
+  }
 };
 
 /*
